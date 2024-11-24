e029b02fdd83f761492330fd6d899e1ddad94a1c
NicknameNeo <abdulloevaslan96@gmail.com>
2024-11-24 08:58:39 +0300

    My project

---
+++ bool 17/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace bool_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var result = (a >= 100 && a <= 999);
+            bool result1 = (a % 2 != 0);
+            
+            
+            Console.WriteLine(result1);
+
+        }
+    }
+}
